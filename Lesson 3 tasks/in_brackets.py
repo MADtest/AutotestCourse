6d8d8d6d21a36418d39c8f95fc0f55da8cbd54f5
MADtest <shim.coma@gmail.com>
2016-10-23 12:28:26 +0300

    HW to l3

---
+++ Lesson 3 tasks/in_brackets.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+def in_brackets(input_string, replacement, all_or_not = 0):
+    starts = []
+    ends = []
+    for i in range(len(input_string)):
+        if input_string[i] == '(':
+            starts.append(i)
+        elif input_string[i] == ')':
+            ends.append(i)
+    end_index = 0
+    for i in range(len(ends)):
+        if ends[i] < starts[i+1]:
+            end_index = i
+            break
+    return input_string.replace(input_string[starts[0]+1:ends[i]], replacement)
+
+if __name__ == '__main__':
+    print in_brackets('dssvf (sdf(rbfdb)(sfsd)ski) fsdgsf (dsgds) ', 'RRRR')
