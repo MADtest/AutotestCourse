7e25f5d48a7d17b5728b4ed3642af8c043bb64b4
MADtest <shim.coma@gmail.com>
2016-10-23 09:33:36 +0300

    HW to l3

--- Lesson 3 tasks/piglatin.py
+++ Lesson 3 tasks/piglatin.py
@@ -1,1 +1,17 @@
 # -*- coding: utf-8 -*-
+
+input_string = raw_input()
+
+words = list(input_string.split(' '))
+
+for i in range(len(words)):
+    if words[i][0] in ('a', 'e', 'i', 'o', 'u'):
+        words[i] += 'way'
+    else:
+        words[i] = words[i][1:] + words[i][0] + 'ay'
+
+result_string = ''
+for i in words:
+    result_string += ' ' + i
+
+print result_string
