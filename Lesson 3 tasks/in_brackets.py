c5ec49f1129de3377cb8178943fcd185ab96e9e6
MADtest <shim.coma@gmail.com>
2016-10-25 21:20:49 +0300

    HW to l3

--- Lesson 3 tasks/in_brackets.py
+++ Lesson 3 tasks/in_brackets.py
@@ -1,34 +1,22 @@
 # -*- coding: utf-8 -*-
 
-def in_brackets(input_string, replacement, all_or_not = 0):
-    last_index = 0
-    starts = [1]
-    ends = [1]
-    while last_index < len(ends):
-        print last_index
-        starts = []
-        ends = []
-        for i in range(len(input_string[last_index:])):
-            if input_string[i] == '(':
-                starts.append(i)
-            elif input_string[i] == ')':
-                ends.append(i)
-        end_index = 0
-        # last_index = 0
 
-        for i in range(len(ends)):
-            if ends[i] < starts[i+1]:
-                end_index = i
-                input_string = input_string.replace(input_string[starts[0] + 1:ends[end_index]], replacement)
-                starts = []
-                ends = []
-                for j in range(len(input_string[last_index:])):
-                    if input_string[j] == ')':
-                        last_index = j+1
-                        # print input_string[last_index:]
-                        break
-                break
-    return input_string
+def in_brackets(input_string, replacement, all_or_not='a'):
+    starts = []
+    ends = []
+    for i in range(len(input_string)):
+        if input_string[i] == '(':
+            starts.append(i)
+        elif input_string[i] == ')':
+            ends.append(i)
+    end_index = 0
+    for i in range(len(ends)):
+        if ends[i] < starts[i+1]:
+            end_index = i
+            break
+    if all_or_not == 'a':
+        return input_string.replace(input_string[starts[0]+1:ends[end_index]], replacement)
 
 if __name__ == '__main__':
-    print in_brackets('dssvf (sdf(rb(jfgx)fdb)(sfsd)ski) fsdgsf (dsgds)mk ', 'RRRR')
+    print in_brackets('dssvf (sdf(rb(jfgx)fdb)(sfsd)ski) fsdgsf (dsgds) ', 'RRRR')
+
