2b34d9725b287296f930f39e66e62ab7f59a164c
MADtest <shim.coma@gmail.com>
2016-10-29 11:14:15 +0300

    HW to l3

--- Lesson 3 tasks/in_brackets.py
+++ Lesson 3 tasks/in_brackets.py
@@ -41,12 +41,31 @@ def in_brackets(input_string, replacement, all_or_not='a'):
         # print allstarts, allends
         result_string = input_string
         for i in range(len(allstarts)):
-            result_string = result_string.replace(input_string[allstarts[i]:allends[i]], '('+replacement+')')
+            result_string = result_string.replace(input_string[allstarts[i]:allends[i]]+')', '('+replacement+')')
             # print '('+input_string[allstarts[i]:allends[i]]+')'
         # for i in allstarts:
         #     input_string.replace(input_string[allstarts[i]:allends[i]], replacement)
         return result_string
+
+
+def in_brackets_new (input_string, replacement, all_or_not='a'):
+    starts = []
+    ends = []
+    for i in range(len(input_string)):
+        if input_string[i] == '(':
+            starts.append(i)
+        elif input_string[i] == ')':
+            ends.append(i)
+    end_index = 0
+    for i in range(len(ends)):
+        if ends[i] < starts[i+1]:
+            end_index = i
+            break
+    if all_or_not == 'a':
+        return input_string.replace('('+input_string[starts[0]+1:ends[end_index]]+')', '('+replacement+')')
+
 if __name__ == '__main__':
 
     print in_brackets('dssvf (sdf(rb(jfgx)fdb)(sfsd)ski) fsdgsf (ds(g)ds) sdf (ds(g)ds) sdf ', 'RRRR', 'b')
+    print in_brackets('sdfsd(sdf)gdfg sdf ((sdf)ert) gdfjsld () as ( ds gds )', 'RRRR', 'b')
 
--- Lesson 4/Solar_system.py
+++ Lesson 4/Solar_system.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import collections, operator, random
 solar_system = {
     'Mercury': 57910000,
     'Venus': 108200000,
@@ -11,18 +11,33 @@ solar_system = {
     'Neptune': 4501000000,
     'Pluto': 5945900000}
 
-sorted_solar_system = {}
-def sorted_dict(dict_to_sort):
-    for i in dict_to_sort.keys().sort:
-        # sorted_solar_system.update(solar_system(i):solar_system.get(i))
-        print solar_system.iteritems()
+
+def system(system_dict):
+    for key, value in system_dict.iteritems():
+        print "Планета {0} находится в {1} км от Солнца".format(key, value)
+
+
+def sort_by_planet(system_dict):
+    sorted_solar_system = collections.OrderedDict(sorted(system_dict.items()))
+    print "Sorted by planet name:"
+    for key, value in sorted_solar_system.iteritems():
+        print "Планета {0} находится в {1} км от Солнца".format(key, value)
+
+
+def sort_by_distance(system_dict):
+    sorted_by_distance = sorted(system_dict.items(), key=operator.itemgetter(1))
+    print "Sorted by distance:"
+    for x, y in sorted_by_distance:
+        print "Планета {0} находится в {1} км от Солнца".format(x, y)
+
+def random_planet_info(system_dict):
+    print "Random planet's info:"
+    key = random.choice(system_dict.keys())
+    print "Планета {0} находится в {1} км от Солнца".format(key, system_dict.get(key))
 
 
 if __name__ == '__main__':
-    for key, value in solar_system.iteritems():
-        print "Планета {0} находится в {1} км от Солнца".format(key, value)
-    # print solar_system.keys().sort
-    sorted_dict(solar_system)
-    print sorted_solar_system
-    # for key, value in solar_system.iteritems():
-    #     print "Планета {0} находится в {1} км от Солнца".format(key, value)
+    system(solar_system)
+    sort_by_planet(solar_system)
+    sort_by_distance(solar_system)
+    random_planet_info(solar_system)
