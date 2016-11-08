ab91448729fe700f988e01ac34e345db8927544e
MADtest <shim.coma@gmail.com>
2016-11-08 21:38:32 +0300

    HW to l3

--- Test1/10.py
+++ Test1/10.py
@@ -1,18 +1,29 @@
 # -*- coding: utf-8 -*-
+import inspect
 
 
 def args_and_result_or_error_to_file(f):
     def log_function(*args, **kwargs):
-        with open('c:\ms\10.txt', 'a') as file:
-            file.write(*args, **kwargs)
-            try:
-                if Exception:
-                    with open('c:\ms\10.txt', 'a') as file:
-                        file.write(Exception)
-                else:
-                    with open('c:\ms\10.txt', 'a') as file:
-                        file.write(f(*args, **kwargs))
+        with open('c:\ms\log10.txt', 'a') as file:
+            file.write(str(inspect.getargspec(f))+'\n')
+            # file.write(str(*args) + ' ' + str(**kwargs))
+        try:
+            file.write(f(*args, **kwargs))
+        except Exception as e:
+            with open('c:\ms\log10.txt', 'a') as file:
+                file.write(str(e)+'\n')
+        else:
+            with open('c:\ms\log10.txt', 'a') as file:
+                file.write(f(*args, **kwargs)+'\n')
+
     return log_function
 
+
+@args_and_result_or_error_to_file
+def any_func(x, y):
+    return x - y
+
 if __name__ == '__main__':
-    pass
+    any_func('asd', 'fsad', 'age', 1243, ['adsf', ''])
+    any_func(5, 8)
+
