89612252cb3586b6b3559502e5423ba5f9fc8b1b
MADtest <shim.coma@gmail.com>
2016-11-08 20:09:57 +0300

    HW to l3

--- Test1/6.py
+++ Test1/6.py
@@ -5,8 +5,17 @@ def add_a_number_to_make_summ_of_elements_equal_zero(list):
     needed = 0
     for i in list:
         needed += i
-    list.append(-1*needed)
+    if needed >= 0:
+        while needed > 9:
+            list.append(-9)
+            needed += -9
+        list.append(-1*needed)
+    elif needed < 0:
+        while needed < -9:
+            list.append(9)
+            needed += 9
+        list.append(-1*needed)
     return list
 
 if __name__ == '__main__':
-    print(add_a_number_to_make_summ_of_elements_equal_zero([5, 7, -9, 8, -99, 56, 56]))
+    print(add_a_number_to_make_summ_of_elements_equal_zero([5, 7, -9, 8, -99, 56, 56, -88]))
