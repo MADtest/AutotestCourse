4ce8c4aadc7390fa3ccd55012e2f593369964417
MADtest <shim.coma@gmail.com>
2016-11-08 19:40:45 +0300

    HW to l3

---
+++ Test1/6.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+
+def add_a_number_to_make_summ_of_elements_equal_zero(list):
+    needed = 0
+    for i in list:
+        needed += i
+    list.append(needed)
+    return list
+
+if __name__ == '__main__':
+    print(add_a_number_to_make_summ_of_elements_equal_zero([5, 7, -9, 8, -99, 56]))
