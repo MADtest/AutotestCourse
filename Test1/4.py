9787e1f872fd732f00f17cbc37ea0f5df87dc72c
MADtest <shim.coma@gmail.com>
2016-11-08 19:28:40 +0300

    HW to l3

--- Test1/4.py
+++ Test1/4.py
@@ -3,9 +3,9 @@
 
 def delete_extra_spaces(string):
     while '  ' in string:
-        string.replace('  ', ' ')
-
+        string = string.replace('  ', ' ')
+    return string
 
 
 if __name__ == '__main__':
-    delete_extra_spaces('jsahfd   iasdfh jasdhfue audhf8ew iahdsf8we   oadsfuoi   oidsauf8 huy8ya  p')
+    delete_extra_spaces('jsahfd   iasdfh jasdhfue audhf8ew iahdsf8we   oadsfuoi   oidsauf8 huy8ya  p  ')
