a8ccbc31ae873f352c3a0582eccb597e75b88f52
MADtest <shim.coma@gmail.com>
2017-01-20 10:12:10 +0300

    a commit

--- lesson 16/HW/test-requests.py
+++ lesson 16/HW/test-requests.py
@@ -1,28 +1,64 @@
 import requests
 import json
+import pytest
 
 
-class Test_user:
+@pytest.fixture
+def user_data():
+    baseUrl = 'http://petstore.swagger.io/v2'
+    header = {'Content-type': 'application/json'}
+    data = {
+            "id": 13,
+            "username": "TestNickName",
+            "firstName": "TestFirstName",
+            "lastName": "TestLastName",
+            "email": "test@test.test",
+            "password": "PASSWORD",
+            "phone": "103",
+            "userStatus": 1
+        }
+    return [baseUrl, header, data]
 
-    def test_create_user(self):
-        self.data = {
-                "id": 13,
-                "username": "TestNickName",
-                "firstName": "TestFirstName",
-                "lastName": "TestLastName",
-                "email": "test@test.test",
-                "password": "PASSWORD",
-                "phone": "03",
-                "userStatus": 0
-            }
-        self.response = requests.get('http://petstore.swagger.io/v2/user/13')
-        # self.header = {'Authorization'}
-        self.baseUrl = 'http://petstore.swagger.io/v2/user/'
-        # self.method = ''  headers=self.header,
-        requests.post(self.baseUrl, data=json.dumps(self.data))
 
-        return self.response.json()
+class TestRequests(object):
+    def test_user_create(self, user_data):
+        method = '/user'
+        response = requests.post(user_data[0] + method, headers=user_data[1], data=json.dumps(user_data[2]))
+        assert response.status_code == 200
 
-if __name__ == '__main__':
-    print Test_user.test_create_user()
-    print requests.get('http://petstore.swagger.io/v2/user/13')
+    def test_user_login(self, user_data):
+        method = '/user/login'
+        response = requests.get(user_data[0] + method, auth=(user_data[2]['username'], user_data[2]['password']))
+        assert response.status_code == 200, "Unexpected response code {0}.".format(response.status_code)
+        assert response.content.find('logged in user session') != -1, "Unexpected response content."
+
+    def test_user_get(self, user_data):
+        method = '/user/' + user_data[2]['username']
+        response = requests.get(user_data[0] + method)
+        assert response.status_code != 400, 'Invalid username supplied'
+        assert response.status_code != 404, 'User not found'
+        assert response.status_code == 200, "Unexpected response code {0}.".format(response.status_code)
+        assert dict(json.loads(response.content)) == user_data[2], 'User data is inconsistent'
+
+    def test_user_update(self, user_data):
+        user_data[2]['lastName'] = 'Married'
+        method = '/user/' + user_data[2]['username']
+        response = requests.put(user_data[0] + method, headers=user_data[1], data=json.dumps(user_data[2]))
+        assert response.status_code != 400, 'Invalid username supplied'
+        assert response.status_code != 404, 'User not found'
+        assert response.status_code == 200, "Unexpected response code {0}.".format(response.status_code)
+
+    def test_user_logout(self, user_data):
+        method = '/user/logout'
+        response = requests.get(user_data[0] + method)
+        assert response.status_code == 200, "Unexpected response code {0}.".format(response.status_code)
+        assert not response.content, "Response body is not empty."
+
+    def test_user_delete(self, user_data):
+        method = '/user/' + user_data[2]['username']
+        response = requests.delete(user_data[0] + method)
+        assert response.status_code != 400, 'Invalid username supplied'
+        assert response.status_code != 404, 'User not found'
+        assert response.status_code == 200, "Unexpected response code {0}.".format(response.status_code)
+        assert not response.content, "Response body is not empty."
+
--- lesson18/test_task.py
+++ lesson18/test_task.py
@@ -1,14 +1,62 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-# class TestLogin:
+class TestLogin:
+#
+# if __name__ == '__main__':
+#     driver = webdriver.Chrome()
+#     driver.get('https://the-internet.herokuapp.com/')
+#     link_auth = driver.find_elements_by_link_text('Form Authentication')
+#     # print link_auth
+#     link_auth[0].click()
+#     username_field = driver.find_element_by_id('username')
+#     password_field = driver.find_element_by_id('username')
+#     driver.quit()
 
-if __name__ == '__main__':
-    driver = webdriver.Chrome()
-    driver.get('https://the-internet.herokuapp.com/')
-    link_auth = driver.find_elements_by_link_text('Form Authentication')
-    # print link_auth
-    link_auth[0].click()
-    username_field = driver.find_element_by_id('username')
-    password_field = driver.find_element_by_id('username')
-    driver.quit()
+    # def test_wait_for_visibility_1(self):
+    #     self.driver = webdriver.Chrome()
+    #     self.driver.get('https://the-internet.herokuapp.com/')
+    #     self.driver.find_element_by_link_text('Dynamic Loading').click()
+    #     self.driver.find_element_by_partial_link_text('Example 1').click()
+    #     finish_messege = self.driver.find_element_by_id('finish')
+    #     assert not finish_messege.is_displayed()
+    #     start_button = self.driver.find_element_by_css_selector('#start button')
+    #     start_button.click()
+    #     wait = WebDriverWait(self.driver, 15)
+    #     wait.until(lambda driver: not start_button.is_displayed())
+    #     assert not start_button.is_displayed()
+    #     wait.until(EC.visibility_of(finish_messege))
+    #     assert finish_messege.is_displayed()
+    #     self.driver.quit()
+    # def is_present(self):
+
+
+    def test_wait_for_visibility_2(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get('https://the-internet.herokuapp.com/')
+        self.driver.find_element_by_link_text('Dynamic Loading').click()
+        self.driver.find_element_by_partial_link_text('Example 2').click()
+        # finish_messege = self.driver.find_element_by_id('finish')
+        # assert not finish_messege.is_enabled()
+        try:
+            len(self.driver.find_elements_by_id('finish')) >= 1
+        except:
+            pass
+        start_button = self.driver.find_element_by_css_selector('#start button')
+        start_button.click()
+        wait = WebDriverWait(self.driver, 15)
+        wait.until(lambda driver: not start_button.is_displayed())
+        assert not start_button.is_displayed()
+        while not self.driver.find_elements_by_id('finish'):
+            try:
+                self.driver.find_element_by_id('finish')
+            except:
+                continue
+        # wait.until(EC.alert_is_present(self.driver.find_element_by_id('finish')))
+        finish_messege = self.driver.find_element_by_id('finish')
+        wait.until(EC.visibility_of(finish_messege))
+        assert finish_messege.is_displayed()
+        self.driver.quit()
