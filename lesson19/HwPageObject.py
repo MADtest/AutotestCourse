bfa1262e614fb0e863c47eeb079a494031529783
MADtest <shim.coma@gmail.com>
2017-01-23 11:21:00 +0300

    Merge branch 'master' of https://github.com/MADtest/AutotestCourse
    # Conflicts:
    #	lesson 16/HW/test-requests.py

--- lesson19/HwPageObject.py
+++ lesson19/HwPageObject.py
@@ -1,2 +1,32 @@
-import selenium
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 import pytest
+
+
+@pytest.fixture
+def data():
+    driver = webdriver.Chrome()
+    baseUrl = 'https://the-internet.herokuapp.com'
+    return [baseUrl, driver]
+
+
+class Test_page_dropdown(object):
+
+    def test_dropdown(self, data):
+        method = '/dropdown'
+        data[1].
+        driver.get(baseUrl + method)
+        dropdown = driver.find_elements_by_id('dropdown')
+        assert dropdown == 1
+
+
+# if __name__ == '__main__':
+#     print Test_page_dropdown.test_dropdown()
+
+# class test_page_dropdown(object):
+
+
+# if __name__ == '__main__':
+#     test_dropdown
+#     print test_dropdown.dropdown
